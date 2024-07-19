184d91f7928e6e041ffb3ef1116df88e3eab7e3a
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2024-07-19 16:09:59 +0100

    Created a Product List helper for testing

---
+++ CheckoutPricing.Test/Helpers/ProductsListHelper.cs
@@ -0,0 +1,44 @@
+﻿using CheckoutPricing.Models;
+
+namespace CheckoutPricing.Test.Helpers
+{
+    static class ProductsListHelper
+    {
+        static List<Product> GetTestProducts()
+        {
+            List<Product> testProducts = new List<Product>();
+
+            testProducts.Add(new Product
+            {
+                SKU = "1",
+                Price = (Decimal)1.00,
+            });
+
+            testProducts.Add(new Product
+            {
+                SKU = "2",
+                Price = (Decimal)6.00,
+            });
+
+            testProducts.Add(new Product
+            {
+                SKU = "3",
+                Price = (Decimal)3.50,
+            });
+
+            testProducts.Add(new Product
+            {
+                SKU = "4",
+                Price = (Decimal)12.99,
+            });
+
+            testProducts.Add(new Product
+            {
+                SKU = "5",
+                Price = (Decimal)7.90,
+            });
+
+            return testProducts;
+        }
+    }
+}
